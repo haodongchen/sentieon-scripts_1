03ae2e95e2a573a52701c8bac9156439cb8ce8a3
Haodong Chen <chen.hdong@gmail.com>
2020-09-05 22:35:40 -0700

    Add more tests

--- triallelic_filter/test_Variant.py
+++ triallelic_filter/test_Variant.py
@@ -6,22 +6,30 @@ class VariantTest(unittest.TestCase):
     def test_init(self):
         line = "chr22\t16059753\trs114433833\tA\tT\t21.77\t.\tAC=2;AF=1.000;" \
           "AN=2;DB;DP=3;ExcessHet=3.0103;FS=0.000;MLEAC=2;MLEAF=1.000;MQ=60" \
-          ";QD=10.88;SOR=0.693\tGT:AD:DP:GQ:PL\t1/1:0,2:2:6:49,6,0\n"
+          "\tGT:AD:DP:GQ:PL\t0/1:0,2:2:6:49,6,0\t0/0:5,0:5:6:0,1,40\n"
         var = Variant(line)
         attr = ['chrom', 'pos', 'id', 'ref', 'alt', 'qual', 'filter']
         val = ['chr22', 16059752, 'rs114433833', 'A', ['T'], 21.77, []]
         for k, v in zip(attr, val):
             assert getattr(var, k) == v
         kw_info = ['AC', 'AF', 'AN', 'DB', 'DP', 'ExcessHet', 'FS', 'MLEAC',
-              'MLEAF', 'MQ', 'QD', 'SOR']
+                   'MLEAF', 'MQ']
         val_info = ['2', '1.000', '2', 'True', '3', '3.0103', '0.000', '2',
-               '1.000', '60', '10.88', '0.693']
+                    '1.000', '60']
         for k, v in zip(kw_info, val_info):
             assert var.info[k] == v
         kw_fmts = ['GT', 'AD', 'DP', 'GQ', 'PL']
-        val_fmts = ['1/1', '0,2', '2', '6', '49,6,0']
-        for k, v in zip(kw_fmts, val_fmts):
-            assert var.samples[0][k] == v
+        val_fmts1 = ['0/1', '0,2', '2', '6', '49,6,0']
+        val_fmts2 = ['0/0', '5,0', '5', '6', '0,1,40']
+        for k, v1, v2 in zip(kw_fmts, val_fmts1, val_fmts2):
+            assert var.samples[0][k] == v1
+            assert var.samples[1][k] == v2
+        assert var.line == line.rstrip()
+
+    def test_init_too_short(self):
+        line = "chr22\t16059753\trs114433833\tA\tT\t21.77\t.\n"
+        with self.assertRaises(IndexError):
+            var = Variant(line)
 
 
 if __name__ == "__main__":
